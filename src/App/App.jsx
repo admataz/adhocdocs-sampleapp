d570d2662c7a14e4e0b2aaa2d0782ce0bb3c0bd0
Adam Davis <adam@admataz.com>
2019-01-13 17:38:17 +0000

    added footer

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,6 +6,7 @@ import { Popover, PopoverBody } from 'reactstrap'
 import FilterPanel from '../components/FilterPanel'
 import DocumentList from '../components/DocumentList'
 import DocumentView from '../components/DocumentView'
+import logo from '../admataz-logo.svg'
 
 const App = () => {
   const [popoverOpen, setSearchPop] = useState(false)
@@ -14,41 +15,46 @@ const App = () => {
   }
 
   return (
-    <div>
-      <nav className="navbar navbar-light bg-light m-0">
-        <Link to="/content" className="btn btn-lg btn-outline-dark text-primary">
-          <FontAwesomeIcon icon={['fas', 'hand-point-right']} className="mx-1" />
-          filter:find
-        </Link>
+    <div className="app-wrapper">
+      <div className="main">
+        <nav className="navbar navbar-light bg-light m-0">
+          <Link to="/content" className="btn btn-lg btn-outline-dark text-primary">
+            <FontAwesomeIcon icon={['fas', 'hand-point-right']} className="mx-1" />
+            filter:find
+          </Link>
 
-        <div className="form-inline">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <a
-            id="searchButton"
-            className="btn btn-outline-success my-2 my-sm-0"
-            data-container="body"
-            data-toggle="popover"
-            data-placement="bottom"
-            data-content="Search not active yet coming soon!"
-          >
-            Search
-          </a>
-          <Popover placement="bottom" isOpen={popoverOpen} target="searchButton" toggle={toggleSearchPop}>
-            <PopoverBody>Coming Soon!</PopoverBody>
-          </Popover>
-        </div>
-      </nav>
+          <div className="form-inline">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <a
+              id="searchButton"
+              className="btn btn-outline-success my-2 my-sm-0"
+              data-container="body"
+              data-toggle="popover"
+              data-placement="bottom"
+              data-content="Search not active yet coming soon!"
+            >
+              Search
+            </a>
+            <Popover placement="bottom" isOpen={popoverOpen} target="searchButton" toggle={toggleSearchPop}>
+              <PopoverBody>Coming Soon!</PopoverBody>
+            </Popover>
+          </div>
+        </nav>
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-sm-6">
-            <Route path="/content(/*)" component={DocumentList} />
-            <Route path="/view/*" component={DocumentView} />
-          </div>
-          <div className="col-sm-6">
-            <Route component={FilterPanel} />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-6">
+              <Route path="/content(/*)" component={DocumentList} />
+              <Route path="/view/*" component={DocumentView} />
+            </div>
+            <div className="col-sm-6">
+              <Route component={FilterPanel} />
+            </div>
           </div>
         </div>
+      </div>
+      <div className="footer mt-1 p-2 bg-light">
+        development by <a href="http://admataz.com">admataz</a>
       </div>
     </div>
   )
